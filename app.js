b1cd98fe3b6226d36f764a4aef5b36521447a49d
jorge <jorgelozada404@gmail.com>
2025-04-29 20:58:29 -0500

    se agrego las tarjetas de los paises

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 import { countries } from './countries.js'
 
-console.log(countries)
+const countries2 = document.querySelector(".countries")
+
+countries.forEach((item) => {
+    const div = document.createElement("div")
+    div.innerHTML = `<div class="country-card">
+                        <h2>${item.country}</h2> 
+                        <hr> Capital: ${item.capital } 
+                        <br>Poblacion: ${item.population }`
+    countries2.appendChild(div)
+
+})
+
+
